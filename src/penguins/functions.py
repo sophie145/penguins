04400c4c2ed4a7625f356891fc73ee685f86981e
Sophie Mason <sophie@Sophies-MacBook-Air-5.local>
2024-11-26 14:44:07 +0000

    created functions file

---
+++ src/penguins/functions.py
@@ -0,0 +1,14 @@
+# converting p-value to works
+def p_value_to_words(p_value):
+
+    p_value_rounded = str(round(p_value,3))
+
+    if p_value >= 0.05:
+        print("we cannot reject the null hypothesis" + p_value_rounded)
+    elif p_value < 0.001:
+        print("*** The p-value is smaller than the criticial threshold, p-value if: " + p_value_rounded)
+    elif p_value < 0.05:
+        print("* The p-value is smaller than the critical threshold, p-value is: " + p_value_rounded)
+    else:
+        print("Your p-value defies maths. I don't know what to do with it.")
+
